bb5fb0617ed7c2d81d541c23f8a6cb3141a53377
Robert Gleason <l2ob122@gmail.com>
2023-03-29 20:58:50 -0700

    added faker information to company

--- src/Company.ts
+++ src/Company.ts
@@ -1,1 +1,19 @@
-class Company {}
+import { faker } from '@faker-js/faker';
+
+export class Company {
+  name: string;
+  catchPhrase: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+
+  constructor() {
+    this.name = faker.company.name();
+    this.catchPhrase = faker.company.catchPhrase();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+}
